b4d96a2dc66ad6fb0f0ab07964411906f0685799
huynhvminhnguyet <49048126+huynhvminhnguyet@users.noreply.github.com>
2020-05-25 23:41:24 +0700

    Exercise for range()
    Số chia hết cho 7 nhưng không phải bội của 5

--- no1_range.py
+++ no1_range.py
@@ -2,3 +2,14 @@
 nằm trong đoạn 2000 và 3200 (tính cả 2000 và 3200). 
 Các số thu được sẽ được in thành chuỗi trên một dòng, cách nhau bằng dấu phẩy.
 '''
+#range(stop) or range(start, stop[, step]): tạo 1 chuỗi các số tăng dần theo giá trị bắt đầu và kết thúc, step: khoảng cách giữa các số trong chuỗi
+#str.join(sequence): nối các phần tử của sequence thành 1 dãy , ngăn cách bởi str
+
+def findMultiplesOfNumber7(mylist):
+    for i in range(2000,3201):
+        if(i % 7 == 0 )and (i % 5 != 0):
+            mylist.append(str(i))
+    print(','.join(mylist))
+
+list1 = []
+findMultiplesOfNumber7(list1)
